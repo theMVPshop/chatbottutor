8f84ca531be6ca5de401241543691c5d5ba5fbe4
Ben Ballard <benb971@gmail.com>
2023-11-02 19:20:48 -0500

    basic stylings

--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -1,41 +1,85 @@
 import { useState } from "react";
+import ChatInput from "./ChatInput.jsx";
+import styled from "styled-components";
 
-const botMessages = ["Message received!", "Wow!", "Heard.", "Okay.", "Alright.", "Interesting.", "Tell me more.", "Oh, really?", "I see."];
+const botMessages = [
+  "Message received!",
+  "Wow!",
+  "Heard.",
+  "Okay.",
+  "Alright.",
+  "Interesting.",
+  "Tell me more.",
+  "Oh, really?",
+  "I see.",
+];
 
 function Chat() {
+  const getBotMessage = () =>
+    botMessages[Math.floor(Math.random() * botMessages.length)];
 
-    const getBotMessage = () => botMessages[Math.floor(Math.random() * botMessages.length)];
+  const [messageInput, setMessageInput] = useState("");
+  const [messages, setMessages] = useState([]); // [{message: "Hello!", sender: "user"}, {message: "Hi!", sender: "bot"}]
 
-    const [messageInput, setMessageInput] = useState("");
-    const [messages, setMessages] = useState([]); // [{message: "Hello!", sender: "user"}, {message: "Hi!", sender: "bot"}]
+  function handleMessageInputChange(e) {
+    setMessageInput(e.target.value);
+  }
 
-    function handleMessageInputChange(e) {
-        setMessageInput(e.target.value);
-    }
+  function onSend(e) {
+    e.preventDefault();
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { text: messageInput, sender: "user" },
+      { text: getBotMessage(), sender: "bot" },
+    ]);
+    setMessageInput("");
+  }
 
-    function onSend(e) {
-        e.preventDefault();
-        setMessages(prevMessages => [...prevMessages, { text: messageInput, sender: "user" }, { text: getBotMessage(), sender: "bot" }]);
-        setMessageInput("");
-    }
-
-    return (
-        <div className="chat-container">
-            <div className="messages-container">
-                {messages.map((message, index) => {
-                    return (
-                        <div key={index} className={`message ${message.sender}`}>
-                            {message.text}
-                        </div>
-                    )
-                })}
-            </div>
-            <form>
-                <input value={messageInput} onChange={handleMessageInputChange} className="message" type="text" />
-                <button onClick={onSend} className="send-button" type="submit">Send</button>
-            </form>
-        </div>
-    )
+  return (
+    <ChatWrap className="chat-container">
+      <Display className="messages-container">
+        {messages.map((message, index) => {
+          return (
+            <Message key={index} className={`message ${message.sender}`}>
+              {message.text}
+            </Message>
+          );
+        })}
+      </Display>
+      <InputWrap>
+        <ChatInput
+          messageInput={messageInput}
+          handleMessageInputChange={handleMessageInputChange}
+          onSend={onSend}
+        />
+      </InputWrap>
+    </ChatWrap>
+  );
 }
 
 export default Chat;
+
+const ChatWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  padding: 20px;
+  background-color: gray;
+`;
+
+const Display = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+const InputWrap = styled.div`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  justify-content: center;
+`;
+
+const Message = styled.div`
+  height: fit-content;
+`;
