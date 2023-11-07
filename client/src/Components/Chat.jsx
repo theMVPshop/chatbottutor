7c7b643cb579c56f2036194f38e8b59b48545e71
ConnorMcGehee <connorjames050@gmail.com>
2023-11-06 19:08:13 -0600

    Support for sockets

--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ChatInput from "./ChatInput.jsx";
 import styled from "styled-components";
+import { socket } from "../socket.js";
 
 const botMessages = [
   "Message received!",
@@ -14,13 +15,64 @@ const botMessages = [
   "I see.",
 ];
 
+const getBotMessage = () =>
+  botMessages[Math.floor(Math.random() * botMessages.length)];
+
 function Chat() {
-  const getBotMessage = () =>
-    botMessages[Math.floor(Math.random() * botMessages.length)];
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
 
+  const [currentResponse, setCurrentResponse] = useState("");
 
+  function sendGPTRequest() {
+    socket.emit('gpt-request', { prompt: gptPrompt });
+  }
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value) {
+      setFooEvents(previous => [...previous, value]);
+    }
+
+    function onGptResponse(data) {
+      console.log('Received a GPT response:', data);
+      setCurrentResponse(prevResponse => prevResponse + data);
+    }
+
+    socket.on('gpt-response', onGptResponse);
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('foo', onFooEvent);
+
+    // Listen for an error event
+    socket.on('gpt-error', (error) => {
+      console.error('Received a GPT error:', error);
+      // Handle error (show error message to the user, etc.)
+    });
+
+    return () => {
+      socket.off('gpt-response', onGptResponse);
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onFooEvent);
+    };
+  }, []);
+
+  const [gptPrompt, setGptPrompt] = useState("");
   const [messageInput, setMessageInput] = useState("");
   const [messages, setMessages] = useState([]); // [{message: "Hello!", sender: "user"}, {message: "Hi!", sender: "bot"}]
+
+
+  function handlePromptChange(e) {
+    setGptPrompt(e.target.value);
+  }
 
   function handleMessageInputChange(e) {
     setMessageInput(e.target.value);
@@ -37,24 +89,34 @@ function Chat() {
   }
 
   return (
-    <ChatWrap className="chat-container">
-      <Display className="messages-container">
-        {messages.map((message, index) => {
-          return (
-            <Message key={index} className={`message ${message.sender}`}>
-              {message.text}
-            </Message>
-          );
-        })}
-      </Display>
-      <InputWrap>
-        <ChatInput
-          messageInput={messageInput}
-          handleMessageInputChange={handleMessageInputChange}
-          onSend={onSend}
-        />
-      </InputWrap>
-    </ChatWrap>
+    <>
+      <div>
+        <label style={{ display: "flex", flexDirection: "column", height: "20rem" }}>
+          Ask GPT
+          <input value={gptPrompt} onChange={handlePromptChange} type="text" /> <br />
+          <button onClick={sendGPTRequest}>Submit</button>
+        </label>
+        {currentResponse}
+      </div>
+      <ChatWrap className="chat-container">
+        <Display className="messages-container">
+          {messages.map((message, index) => {
+            return (
+              <Message key={index} className={`message ${message.sender}`}>
+                {message.text}
+              </Message>
+            );
+          })}
+        </Display>
+        <InputWrap>
+          <ChatInput
+            messageInput={messageInput}
+            handleMessageInputChange={handleMessageInputChange}
+            onSend={onSend}
+          />
+        </InputWrap>
+      </ChatWrap>
+    </>
   );
 
 }
