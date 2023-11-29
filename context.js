ef16e1b07a63c898811a1da305131a77c8f120db
DKYoungOne <bawsinator@gmail.com>
2023-11-28 16:53:15 -0800

    Add context file with course context strings, utilize imports to access context strings in server.js

---
+++ context.js
@@ -0,0 +1,89 @@
+export const context311 = `1. Node.js
+2. Test software: Postman
+3. Program using remote API
+4. Practice passing Jest Tests
+5. Using Axios as an HTTP Client
+6. Using Express.js to build JavaScript servers
+7. Using Postman as a testing software
+8. Building routers and controllers with Express.js
+9. Saving data to and generating a static file
+10. Creating a database instance on Google Cloud
+11. Using MySQL Workbench to test and query their database
+12. Advanced SELECT queries using multiple tables
+13. How to use GROUP BY & HAVING
+14. The remaining CRUD operations in SQL: Create, Update, and Delete
+15. How to use NPM MySQL with Express to connect to an SQL database
+16. Using an Agile approach to build an API as a team
+17. The effectiveness of scrum as it pertains to working in a team on a singular project
+18. Using Google Cloud to create an instance of a database
+19. Configuring and importing data
+20. How to create a visual aid for data modeling, ER diagram
+21. JWT Authentication
+22. Logging in to a server
+23. Using Postman to test their authentication route
+24. Deploying an API and database to the cloud via App Engine
+25. The use of cloud storage as it pertains to websites and web apps
+26. CI/CD using CloudBees CodeShip
+27. Synthesizing their knowledge of SQL into building their own app
+28. Communicating to others about web technology`;
+
+export const context411 = `Current Objectives: 1. How to use React to create a simple composable web app
+2. Demonstrate their ability to build a new todo app in React from recollection
+3. Passing data between components using props
+4. Set state and use it
+5. Fetch external data and set state with it
+6. Dynamically render data with uniform elements
+7. Conditional rendering with React
+8. Initiating and using local state in React components
+9. Passing data via props to child components
+10. Developing an app in collaboration
+11. API requests, conditional rendering, mapping, forms
+12. Programming in a group
+13. Using React LifeCycle Methods
+14. Using styled components with Material UI
+15. Programming in a group
+16. Using React LifeCycle Methods
+17. Using styled components with Material UI
+18. Client-side routing of a web app with React Router
+19. The NPM package React-Router with its dependencies and structure
+20. Using Boolean logic to allow a user access to specific components
+21. Setting cookies in the user's browser
+22. Global state over an application using Redux
+23. Managing global state of an application with Redux
+24. Asynchronous calls with Redux-Thunk
+25. Planning an app
+26. Collaborating with a partner in App Design
+27. Data management
+28. Critically thinking through the flow of user with conditional paths
+29. Reusability, as it relates to programming
+30. Thinking ahead to solve potential problems
+Previous Objectives:
+Class 311:
+1. Node.js
+2. Test software: Postman
+3. Program using remote API
+4. Practice passing Jest Tests
+5. Using Axios as an HTTP Client
+6. Using Express.js to build JavaScript servers
+7. Using Postman as a testing software
+8. Building routers and controllers with Express.js
+9. Saving data to and generating a static file
+10. Creating a database instance on Google Cloud
+11. Using MySQL Workbench to test and query their database
+12. Advanced SELECT queries using multiple tables
+13. How to use GROUP BY & HAVING
+14. The remaining CRUD operations in SQL: Create, Update, and Delete
+15. How to use NPM MySQL with Express to connect to an SQL database
+16. Using an Agile approach to build an API as a team
+17. The effectiveness of scrum as it pertains to working in a team on a singular project
+18. Using Google Cloud to create an instance of a database
+19. Configuring and importing data
+20. How to create a visual aid for data modeling, ER diagram
+21. JWT Authentication
+22. Logging in to a server
+23. Using Postman to test their authentication route
+24. Deploying an API and database to the cloud via App Engine
+25. The use of cloud storage as it pertains to websites and web apps
+26. CI/CD using CloudBees CodeShip
+27. Synthesizing their knowledge of SQL into building their own app
+28. Communicating to others about web technology`;
